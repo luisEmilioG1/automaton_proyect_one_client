cd2dbeb84de002c6b029b75b5ca7f10d9e3fcc04
Luis Emilio Grijalba <luisegrijalba8@gmail.com>
2022-09-24 17:41:59 -0500

    load and unio with api REST

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
 import { Graphviz } from "graphviz-react";
+import { load, union } from "./api/automatonApi";
 
+import "./App.css";
 function App() {
-  const graphics = document.querySelectorAll("svg");
-  if (graphics.length) {
-    console.log(graphics);
-    graphics.array.forEach((element) => {
-      console.log(element.width);
-    });
+  const [automatonOneFile, setAutomatonOneFile] = useState();
+  const [automatonTwoFile, setAutomatonTwoFile] = useState();
+  const [dotAutomatonOne, setDotAutomatonUne] = useState();
+  const [dotAutomatonTwo, setDotAutomatonTwo] = useState(`
+  initial [label="", shape="plaintext", style="dotted"]
+  B [color=green]
+  initial -> A
+  A -> A [label="y"]
+  A -> B [label="x"] 
+  B -> B [label="x,y"]
+  `);
+
+  async function loadAutomaton(event) {
+    const file = event.target.files[0];
+
+    const formData = new FormData();
+    formData.append("quintuple", file);
+
+    const { message, automaton } = await load(formData);
+
+    if (!automaton) {
+      alert(message);
+      return;
+    }
+
+    if (event.target.name === "loadOne") {
+      setAutomatonOneFile(file);
+      //setDotAutomatonUne(automaton);
+    } else {
+      setAutomatonTwoFile(file);
+      //setDotAutomatonTwo(automaton);
+    }
   }
+
+  async function unionOperation() {
+    const body = new FormData();
+    if (!automatonOneFile || !automatonTwoFile) {
+      alert("datos incompletos para hacer unión");
+      return;
+    }
+    body.append("quintuple_one", automatonOneFile);
+    body.append("quintuple_two", automatonTwoFile);
+
+    const { message, automaton_union } = await union(body);
+
+    if (!automaton_union) {
+      alert(message);
+    }
+
+    console.log(automaton_union);
+  }
+
+  useEffect(() => {
+    console.log(automatonOneFile, automatonTwoFile);
+  }, [automatonOneFile, automatonTwoFile]);
 
   return (
     <div className="App">
       <div>
         <div className="buttons">
-          <button type="button">cargar</button>
+          <button type="button">
+            <label>
+              cargar
+              <input name="loadOne" type="file" onChange={loadAutomaton} />
+            </label>
+          </button>
         </div>
         <Graphviz
           dot={`digraph {
   "pep" [label="Nodo", fillcolor="lightblue", style="filled", shape="triangle"] 
+  "p" [label="w", fillcolor="white", style="filled", shape="circle"] 
   sldjfsjdl -> b[label="0.2",weight="0.2",color=red,penwidth=3.0];
   "pep" -> c[label="0.4",weight="0.4"];
   c -> b[label="0.6",weight="0.6"];
@@ -32,28 +88,26 @@ function App() {
       </div>
       <div>
         <div className="buttons">
-          <button type="button">cargar</button>
+          <button type="button">
+            <label>
+              cargar
+              <input name="loadTwo" type="file" onChange={loadAutomaton} />
+            </label>
+          </button>
         </div>
 
-        <Graphviz
-          dot={`digraph {
-  sldjfsjdl -> b[label="0.2",weight="0.2",color=red,penwidth=3.0];
-  a -> c[label="0.4",weight="0.4"];
-  c -> b[label="0.6",weight="0.6"];
-  c -> e[label="0.6",weight="0.6"];
-  e -> e[label="0.1",weight="0.1"];
-  e -> b[label="0.7",weight="0.7"];
-  b -> e;
-}`}
-        />
+        <Graphviz dot={`digraph {${dotAutomatonTwo}}`} />
       </div>
       <div>
         <div className="buttons">
-          <button type="button">unión</button>
+          <button type="button" onClick={unionOperation}>
+            unión
+          </button>
           <button type="button">intersección</button>
           <button type="button">complemento uno</button>
           <button type="button">complemento dos</button>
-          <button type="button">reverso</button>
+          <button type="button">reverso uno</button>
+          <button type="button">reverso dos</button>
         </div>
 
         <Graphviz
---
+++ src/api/automatonApi.js
@@ -0,0 +1,24 @@
+export async function load(bodyForm) {
+  const options = {
+    method: "POST",
+    body: bodyForm,
+  };
+
+  const response = await fetch("http://127.0.0.1:5000/load", options);
+
+  const responseJson = await response.json();
+
+  return responseJson;
+}
+
+export async function union(bodyForm) {
+  const options = {
+    method: "POST",
+    body: bodyForm,
+  };
+
+  const response = await fetch("http://127.0.0.1:5000/union", options);
+  const responseJson = await response.json();
+
+  return responseJson;
+}
